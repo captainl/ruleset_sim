1c6829e74db6e6e9b1d789ccc66264832cea44cf
captainl <lrtrybuch@gmail.com>
2020-09-23 23:04:40 -0700

    Updates

---
+++ precache-manifest.55c8a98430d0f47360da73d52ab81447.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cc1e48563d832bad36c32978af7ba6",
+    "url": "/ruleset_sim/index.html"
+  },
+  {
+    "revision": "0e977fcbd93a81706984",
+    "url": "/ruleset_sim/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c26c181884044605cad0",
+    "url": "/ruleset_sim/static/js/2.c7e12041.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ruleset_sim/static/js/2.c7e12041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e977fcbd93a81706984",
+    "url": "/ruleset_sim/static/js/main.b554ca3b.chunk.js"
+  },
+  {
+    "revision": "c261927b6f5823bf13c9",
+    "url": "/ruleset_sim/static/js/runtime-main.ae5124da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ruleset_sim/static/media/logo.5d5d9eef.svg"
+  }
+]);
